fixed matching and working on getting efficiency calculation

src/data/delphes/convert_to_h5.py
@@ -8,10 +8,17 @@ import numpy as np
 import uproot
 import vector
 
+import matplotlib.pyplot as plt
+import mplhep as hep
+import hist
+from cycler import cycler
+plt.style.use(hep.style.CMS)
+plt.rcParams.update({'font.size': 20})
+cmap_petroff10 = ["#3f90da", "#ffa90e", "#bd1f01", "#94a4a2", "#832db6", "#a96b59", "#e76300", "#b9ac70", "#717581", "#92dadd"]
+plt.rcParams.update({"axes.prop_cycle": cycler("color", cmap_petroff10)})
+
 from src.data.delphes.matching import (
     match_fjet_to_jet,
-    # match_higgs_to_fjet,
-    # match_higgs_to_jet,
     match_top_to_fjet,
     match_top_to_jet,
 )
@@ -146,46 +153,51 @@ def get_datasets(arrays, n_tops):  # noqa: C901
         with_name="Momentum4D",
     )
 
-    # tops_condition = np.logical_and(
-    #     np.abs(particles.pid) == 6, np.logical_and(
-    #         np.abs(particles.pid[particles.d1]) == 5, np.abs(particles.pid[particles.d2]) == 24
-    #     )   # do we know the bquarks are going to be daughter 1? yes, confirmed.
-    # )
-    # topquarks = ak.to_regular(particles[tops_condition], axis=1)
-    # topquark_idx_sort = ak.argsort(topquarks.idx, axis=-1)
-    # topquarks = ak.to_regular(topquarks[topquark_idx_sort])
+    tops_condition = np.logical_and(
+        np.abs(particles.pid) == 6, np.logical_and(
+            np.abs(particles.pid[particles.d1]) == 5, np.abs(particles.pid[particles.d2]) == 24
+        )   # do we know the bquarks are going to be daughter 1? yes, confirmed.
+    )
+    topquarks = ak.to_regular(particles[tops_condition], axis=1)
+    topquark_idx_sort = ak.argsort(topquarks.idx, axis=-1)
+    topquarks = ak.to_regular(topquarks[topquark_idx_sort])
+    print(f"topquarks statuses = \n{topquarks.status}")
+
+    bquarks = ak.to_regular(
+        final_particle(
+            5, 6, particles, 
+            # final_status=52
+        ), axis=1
+    )
+    bquarks = ak.to_regular(bquarks[topquark_idx_sort])
+    print(f"bquarks statuses = \n{bquarks.status}")
+
+    wbosons = ak.to_regular(
+        final_particle(
+            24, 6, particles, 
+            # final_status=52
+        ), axis=1
+    )
+    wbosons = ak.to_regular(wbosons[topquark_idx_sort])
+    print(f"wbosons statuses = \n{wbosons.status}")
     
-    # print(f"topquarks statuses = \n{topquarks.status}")
-    # print('gets to bquarks')
-
-    # bquarks = ak.to_regular(final_particle(5, 6, particles, final_status=52), axis=1)
-    # bquarks = ak.to_regular(bquarks[topquark_idx_sort])
-
-    # print(f"bquarks statuses = \n{bquarks.status}")
-    # print('gets to wbosons')
-
-    # wbosons = ak.to_regular(final_particle(24, 6, particles, final_status=52), axis=1)
-    # wbosons = ak.to_regular(wbosons[topquark_idx_sort])
-
-    # print(f"wbosons statuses = \n{wbosons.status}")
-    # print('gets to wquarks_d1')
+    wquarks_d1 = ak.to_regular(
+        final_particle(
+            np.abs(ak.to_regular(particles.pid[wbosons.d1], axis=1)), None, particles, 
+            # final_status=52, 
+            intermediate_particles=particles[wbosons.d1]
+        ), axis=1
+    )
+    print(f"wquarks_d1 statuses = \n{wquarks_d1.status}")
+    wquarks_d2 = ak.to_regular(
+        final_particle(
+            np.abs(ak.to_regular(particles.pid[wbosons.d2], axis=1)), None, particles, 
+            # final_status=52, 
+            intermediate_particles=particles[wbosons.d2]
+        ), axis=1
+    )
     
-    # wquarks_d1 = ak.to_regular(
-    #     final_particle(
-    #         np.abs(ak.to_regular(particles.pid[wbosons.d1], axis=1)), None, particles, 
-    #         final_status=52, intermediate_particles=particles[wbosons.d1]
-    #     ), axis=1
-    # )
-    # print(f"wquarks_d1 statuses = \n{wquarks_d1.status}")
-    # print('gets to wquarks_d2')
-    # wquarks_d2 = ak.to_regular(
-    #     final_particle(
-    #         np.abs(ak.to_regular(particles.pid[wbosons.d2], axis=1)), None, particles, 
-    #         final_status=52, intermediate_particles=particles[wbosons.d2]
-    #     ), axis=1
-    # )
-    
-    # print(f"wquarks_d2 statuses = \n{wquarks_d2.status}")
+    print(f"wquarks_d2 statuses = \n{wquarks_d2.status}")
 
     # print(f"all topquarks statuses = \n{particles.status[tops_condition]}")
     # bquarks_condition = np.logical_and(
@@ -308,7 +320,7 @@ def get_datasets(arrays, n_tops):  # noqa: C901
         with_name="Momentum4D",
     )
 
-    print(f"id of min deltaR \n{particles.pid[np.argmin(particles.deltaR(jets))]}")
+    # print(f"id of min deltaR \n{particles.pid[np.argmin(particles.deltaR(jets))]}")
 
 
     # print(ak.type(arrays["Jet/Jet.Particles"].refs))
@@ -466,7 +478,7 @@ def get_datasets(arrays, n_tops):  # noqa: C901
     print(f"number of reco. 2 tops semi-resolved (bq or qq) events = {ak.sum(two_semiResolved)}")
 
     # one fully resolved top, one fully-boosted top
-    one_fullyResolved_one_bqqFjet = (
+    one_fullyResolved_one_fullyBoosted = (
         (
             (ak.sum(top_idx == 2, axis=1) == 3)
             & (ak.sum(fj_top_bqq_idx == 1, axis=1) == 1)
@@ -475,7 +487,7 @@ def get_datasets(arrays, n_tops):  # noqa: C901
             & (ak.sum(fj_top_bqq_idx == 2, axis=1) == 1)
         )
     )
-    print(f"number of reco. 1 top fully-resolved, 1 top fully-boosted events = {ak.sum(one_fullyResolved_one_bqqFjet)}")
+    print(f"number of reco. 1 top fully-resolved, 1 top fully-boosted events = {ak.sum(one_fullyResolved_one_fullyBoosted)}")
     
     # one semi-resolved top, one fully-boosted top
     one_bqFjet_one_bqqFjet = (
@@ -518,6 +530,13 @@ def get_datasets(arrays, n_tops):  # noqa: C901
         )
     )
     print(f"number of reco. 2 tops fully-boosted events = {ak.sum(two_fullyBoosted)}")
+
+    num_proper_events = (
+        ak.sum(two_fullyResolved) + ak.sum(one_fullyResolved_one_semiResolved) + ak.sum(one_fullyResolved_one_fullyBoosted) 
+        + ak.sum(two_semiResolved) + ak.sum(one_semiResolved_one_fullyBoosted)
+        + ak.sum(two_fullyBoosted)
+    )
+    print(f"number of good reco events = {num_proper_events}")
 
     # number matched bjets
     matched_bjet = (
@@ -563,19 +582,100 @@ def get_datasets(arrays, n_tops):  # noqa: C901
     # print(ak.type(wquarks_d1))
     # print(ak.type(bquarks))
     # print(f"always 2 bquarks? = {ak.all(ak.num(bquarks) == 2)}")
-    print(f"min DeltaR btwn bquark1 and jets = {ak.min(bquarks[ak.local_index(bquarks) == 0].deltaR(jets), axis=1)}")
-    print(f"min DeltaR btwn bquark2 and jets = {ak.min(bquarks[ak.local_index(bquarks) == 1].deltaR(jets), axis=1)}")
-    print(f"min DeltaR btwn w1quark1 and jets = {ak.min(wquarks_d1[ak.local_index(wquarks_d1) == 0].deltaR(jets), axis=1)}")
-    print(f"min DeltaR btwn w1quark2 and jets = {ak.min(wquarks_d2[ak.local_index(wquarks_d2) == 0].deltaR(jets), axis=1)}")
-    print(f"min DeltaR btwn w2quark1 and jets = {ak.min(wquarks_d1[ak.local_index(wquarks_d1) == 1].deltaR(jets), axis=1)}")
-    print(f"min DeltaR btwn w2quark2 and jets = {ak.min(wquarks_d2[ak.local_index(wquarks_d2) == 1].deltaR(jets), axis=1)}")
+    min_dR_bquark1 = ak.min(bquarks[ak.local_index(bquarks) == 0].deltaR(jets), axis=1)
+    print(f"min DeltaR btwn bquark1 and jets = {min_dR_bquark1}")
+    min_dR_bquark2 = ak.min(bquarks[ak.local_index(bquarks) == 1].deltaR(jets), axis=1)
+    print(f"min DeltaR btwn bquark2 and jets = {min_dR_bquark2}")
+    min_dR_w1quark1 = ak.min(wquarks_d1[ak.local_index(wquarks_d1) == 0].deltaR(jets), axis=1)
+    print(f"min DeltaR btwn w1quark1 and jets = {min_dR_w1quark1}")
+    min_dR_w1quark2 = ak.min(wquarks_d2[ak.local_index(wquarks_d2) == 0].deltaR(jets), axis=1)
+    print(f"min DeltaR btwn w1quark2 and jets = {min_dR_w1quark2}")
+    min_dR_w2quark1 = ak.min(wquarks_d1[ak.local_index(wquarks_d1) == 1].deltaR(jets), axis=1)
+    print(f"min DeltaR btwn w2quark1 and jets = {min_dR_w2quark1}")
+    min_dR_w2quark2 = ak.min(wquarks_d2[ak.local_index(wquarks_d2) == 1].deltaR(jets), axis=1)
+    print(f"min DeltaR btwn w2quark2 and jets = {min_dR_w2quark2}")
 
-    print(f"min Delta pT btwn bquark1 and jets = {ak.min(ak.where(bquarks.pt[ak.local_index(bquarks) == 0] - jets.pt > 0, bquarks.pt[ak.local_index(bquarks) == 0] - jets.pt, -(bquarks.pt[ak.local_index(bquarks) == 0] - jets.pt)), axis=1)}")
-    print(f"min Delta pT btwn bquark2 and jets = {ak.min(ak.where(bquarks.pt[ak.local_index(bquarks) == 1] - jets.pt > 0, bquarks.pt[ak.local_index(bquarks) == 1] - jets.pt, -(bquarks.pt[ak.local_index(bquarks) == 1] - jets.pt)), axis=1)}")
-    print(f"min Delta pT btwn w1quark1 and jets = {ak.min(ak.where(wquarks_d1.pt[ak.local_index(wquarks_d1) == 0] - jets.pt > 0, wquarks_d1.pt[ak.local_index(wquarks_d1) == 0] - jets.pt, -(wquarks_d1.pt[ak.local_index(wquarks_d1) == 0] - jets.pt)), axis=1)}")
-    print(f"min Delta pT btwn w1quark2 and jets = {ak.min(ak.where(wquarks_d2.pt[ak.local_index(wquarks_d2) == 0] - jets.pt > 0, wquarks_d2.pt[ak.local_index(wquarks_d2) == 0] - jets.pt, -(wquarks_d2.pt[ak.local_index(wquarks_d2) == 0] - jets.pt)), axis=1)}")
-    print(f"min Delta pT btwn w2quark1 and jets = {ak.min(ak.where(wquarks_d1.pt[ak.local_index(wquarks_d1) == 1] - jets.pt > 0, wquarks_d1.pt[ak.local_index(wquarks_d1) == 1] - jets.pt, -(wquarks_d1.pt[ak.local_index(wquarks_d1) == 1] - jets.pt)), axis=1)}")
-    print(f"min Delta pT btwn w2quark2 and jets = {ak.min(ak.where(wquarks_d2.pt[ak.local_index(wquarks_d2) == 1] - jets.pt > 0, wquarks_d2.pt[ak.local_index(wquarks_d2) == 1] - jets.pt, -(wquarks_d2.pt[ak.local_index(wquarks_d2) == 1] - jets.pt)), axis=1)}")
+    
+    def weights_pt_hist(data, pt, n_bins: int=35, min_pt: float=0., max_pt: float=350.):
+        bin_edges = np.array([(i * (max_pt-min_pt) / n_bins) + min_pt for i in range(n_bins+1)])
+
+        weights = np.array([1. for _ in range(np.shape(data)[0])])
+        for i in range(n_bins):
+            mask_arr = np.logical_and(pt >= bin_edges[i], pt < bin_edges[i+1])
+            weights[mask_arr] = weights[mask_arr] * (np.sum(data[mask_arr]) / np.sum(mask_arr)) / np.sum(mask_arr)
+        
+        return weights
+    
+    fig, ax = plt.subplots()
+    hep.cms.text("Work in Progress", ax=ax)
+    n_bins, min_pt, max_pt = 35, 0., 350.
+    hist_axis = hist.axis.Regular(n_bins, min_pt, max_pt, name='var', label=r'$p_T$', growth=False, underflow=False, overflow=False)
+    bquark1_hist = hist.Hist(hist_axis, storage='weight').fill(var=bquarks.pt[ak.local_index(bquarks) == 0], weight=weights_pt_hist(min_dR_bquark1, bquarks.pt[ak.local_index(bquarks) == 0], n_bins, min_pt, max_pt))
+    bquark2_hist = hist.Hist(hist_axis, storage='weight').fill(var=bquarks.pt[ak.local_index(bquarks) == 1], weight=weights_pt_hist(min_dR_bquark2, bquarks.pt[ak.local_index(bquarks) == 1], n_bins, min_pt, max_pt))
+    w1quark1_hist = hist.Hist(hist_axis, storage='weight').fill(var=wquarks_d1.pt[ak.local_index(wquarks_d1) == 0], weight=weights_pt_hist(min_dR_w1quark1, wquarks_d1.pt[ak.local_index(wquarks_d1) == 0], n_bins, min_pt, max_pt))
+    w1quark2_hist = hist.Hist(hist_axis, storage='weight').fill(var=wquarks_d2.pt[ak.local_index(wquarks_d2) == 0], weight=weights_pt_hist(min_dR_w1quark2, wquarks_d2.pt[ak.local_index(wquarks_d2) == 0], n_bins, min_pt, max_pt))
+    w2quark1_hist = hist.Hist(hist_axis, storage='weight').fill(var=wquarks_d1.pt[ak.local_index(wquarks_d1) == 1], weight=weights_pt_hist(min_dR_w2quark1, wquarks_d1.pt[ak.local_index(wquarks_d1) == 1], n_bins, min_pt, max_pt))
+    w2quark2_hist = hist.Hist(hist_axis, storage='weight').fill(var=wquarks_d2.pt[ak.local_index(wquarks_d2) == 1], weight=weights_pt_hist(min_dR_w2quark2, wquarks_d2.pt[ak.local_index(wquarks_d2) == 1], n_bins, min_pt, max_pt))
+    hep.histplot(
+        [bquark1_hist, bquark2_hist, w1quark1_hist, w1quark2_hist, w2quark1_hist, w2quark2_hist], 
+        yerr=True, alpha=0.5, histtype='step', label=['bquark1', 'bquark2', 'w1quark1', 'w1quark2', 'w2quark1', 'w2quark2']
+    )
+    ax.legend()
+    ax.set_ylabel('Average min($\Delta R$)')
+    plt.savefig('avg_min_deltaR_against_pt.png')
+    plt.close()
+
+    n_pt_bins, min_pt, max_pt = 35, 0., 350.
+    n_dR_bins, min_dR, max_dR = 15, 0., 1.5
+    for plot, pt_data, dR_data in [
+        ('bquark1', bquarks.pt[ak.local_index(bquarks) == 0], min_dR_bquark1), ('bquark2', bquarks.pt[ak.local_index(bquarks) == 1], min_dR_bquark2),
+        ('w1quark1', wquarks_d1.pt[ak.local_index(wquarks_d1) == 0], min_dR_w1quark1), ('w1quark2', wquarks_d2.pt[ak.local_index(wquarks_d2) == 0], min_dR_w1quark2),
+        ('w2quark1', wquarks_d1.pt[ak.local_index(wquarks_d1) == 1], min_dR_w2quark1), ('w2quark2', wquarks_d2.pt[ak.local_index(wquarks_d2) == 1], min_dR_w2quark2),
+    ]:
+        fig, ax = plt.subplots()
+        hep.cms.text("Work in Progress", ax=ax)
+        
+        pt_axis = hist.axis.Regular(n_pt_bins, min_pt, max_pt, name='pt_var', label=r'$p_T$', growth=False, underflow=False, overflow=False)
+        deltaR_axis = hist.axis.Regular(n_dR_bins, min_dR, max_dR, name='dr_var', label=r'$\Delta R$', growth=False, underflow=False, overflow=False)
+        hist_data = hist.Hist(pt_axis, deltaR_axis).fill(pt_var=pt_data, dr_var=dR_data)
+        hep.hist2dplot(
+            hist_data, ax=ax,
+        )
+        plt.savefig(f'{plot}_min_deltaR_against_pt.png')
+        plt.close()
+
+    
+
+    print('-'*60)
+    deltaR_cut = 0.5
+    frac_bquark1 = ak.sum(ak.min(bquarks[ak.local_index(bquarks) == 0].deltaR(jets), axis=1) < deltaR_cut) / ak.num(top_idx, axis=0)
+    print(f"frac bquark1 within {deltaR_cut} = {frac_bquark1}")
+    frac_bquark2 = ak.sum(ak.min(bquarks[ak.local_index(bquarks) == 1].deltaR(jets), axis=1) < deltaR_cut) / ak.num(top_idx, axis=0)
+    print(f"frac bquark2 within {deltaR_cut} = {frac_bquark2}")
+    frac_w1quark1 = ak.sum(ak.min(wquarks_d1[ak.local_index(wquarks_d1) == 0].deltaR(jets), axis=1) < deltaR_cut) / ak.num(top_idx, axis=0)
+    print(f"frac w1quark1 within {deltaR_cut} = {frac_w1quark1}")
+    frac_w1quark2 = ak.sum(ak.min(wquarks_d2[ak.local_index(wquarks_d2) == 0].deltaR(jets), axis=1) < deltaR_cut) / ak.num(top_idx, axis=0)
+    print(f"frac w1quark2 within {deltaR_cut} = {frac_w1quark2}")
+    frac_w2quark1 = ak.sum(ak.min(wquarks_d1[ak.local_index(wquarks_d1) == 1].deltaR(jets), axis=1) < deltaR_cut) / ak.num(top_idx, axis=0)
+    print(f"frac w2quark1 within {deltaR_cut} = {frac_w2quark1}")
+    frac_w2quark2 = ak.sum(ak.min(wquarks_d2[ak.local_index(wquarks_d2) == 1].deltaR(jets), axis=1) < deltaR_cut) / ak.num(top_idx, axis=0)
+    print(f"frac w2quark2 within {deltaR_cut} = {frac_w2quark2}")
+    expected_resolved_efficiency = frac_bquark1 * frac_bquark2 * frac_w1quark1 * frac_w1quark2 * frac_w2quark1 * frac_w2quark2
+    print(f"expected fully-resolved efficiency at dR {deltaR_cut} = {expected_resolved_efficiency}")
+
+    print('-'*60)
+    print(f"w2quark1 deltaR with jets: {wquarks_d2[0, 1].deltaR(jets[0])}")
+    print(f"w2quark1.m1 deltaR with jets: {particles[wquarks_d2.m1][0, 1].deltaR(jets[0])}")
+    print(f"w2quark1.m1 status: {particles.status[wquarks_d2.m1][0, 1]}")
+    print(f"w2quark1.m1.m1 deltaR with jets: {particles[particles.m1[wquarks_d2.m1]][0, 1].deltaR(jets[0])}")
+    print(f"w2quark1.m1.m1 status: {particles.status[particles.m1[wquarks_d2.m1]][0, 1]}")
+
+    # print(f"min Delta pT btwn bquark1 and jets = {ak.min(ak.where(bquarks.pt[ak.local_index(bquarks) == 0] - jets.pt > 0, bquarks.pt[ak.local_index(bquarks) == 0] - jets.pt, -(bquarks.pt[ak.local_index(bquarks) == 0] - jets.pt)), axis=1)}")
+    # print(f"min Delta pT btwn bquark2 and jets = {ak.min(ak.where(bquarks.pt[ak.local_index(bquarks) == 1] - jets.pt > 0, bquarks.pt[ak.local_index(bquarks) == 1] - jets.pt, -(bquarks.pt[ak.local_index(bquarks) == 1] - jets.pt)), axis=1)}")
+    # print(f"min Delta pT btwn w1quark1 and jets = {ak.min(ak.where(wquarks_d1.pt[ak.local_index(wquarks_d1) == 0] - jets.pt > 0, wquarks_d1.pt[ak.local_index(wquarks_d1) == 0] - jets.pt, -(wquarks_d1.pt[ak.local_index(wquarks_d1) == 0] - jets.pt)), axis=1)}")
+    # print(f"min Delta pT btwn w1quark2 and jets = {ak.min(ak.where(wquarks_d2.pt[ak.local_index(wquarks_d2) == 0] - jets.pt > 0, wquarks_d2.pt[ak.local_index(wquarks_d2) == 0] - jets.pt, -(wquarks_d2.pt[ak.local_index(wquarks_d2) == 0] - jets.pt)), axis=1)}")
+    # print(f"min Delta pT btwn w2quark1 and jets = {ak.min(ak.where(wquarks_d1.pt[ak.local_index(wquarks_d1) == 1] - jets.pt > 0, wquarks_d1.pt[ak.local_index(wquarks_d1) == 1] - jets.pt, -(wquarks_d1.pt[ak.local_index(wquarks_d1) == 1] - jets.pt)), axis=1)}")
+    # print(f"min Delta pT btwn w2quark2 and jets = {ak.min(ak.where(wquarks_d2.pt[ak.local_index(wquarks_d2) == 1] - jets.pt > 0, wquarks_d2.pt[ak.local_index(wquarks_d2) == 1] - jets.pt, -(wquarks_d2.pt[ak.local_index(wquarks_d2) == 1] - jets.pt)), axis=1)}")
 
     # keep events with >= min_jets small-radius jets
     min_jets = 3 * n_tops

src/data/delphes/matching.py
@@ -139,29 +139,39 @@ def match_top_to_fjet(
 #         alljet_builder.begin_list()
 #         bjet_builder.begin_list()
 #         wjet_builder.begin_list()
-#         for j, (bquark, wquark1, wquark1_pid, wquark2, wquark2_pid) in enumerate(zip(
-#             bquarks_event,
-#             wquarks1_event, wquarks1_event.pid, 
-#             wquarks2_event, wquarks2_event.pid
-#         )):  # dont need to check b and w mother index b/c made them match by construction
-            
-#             mindeltaR, mindeltaR_idxs = 999, (0, 0)  # mindeltaR, (mindeltaR_topidx, mindeltaR_quarktype)
-#             for i, (jet, jet_flav) in enumerate(zip(jets_event, jets_event.flavor)):
-#                 alljet_match_idx, bjet_match_idx, wjet_match_idx = -1, -1, -1
-            
-#                 # bquark_deltaR = jet.deltaR(bquark) if jet.deltaR(bquark) < JET_DR and np.abs(jet_flav) == 5 else 999
-#                 # wquark1_deltaR = jet.deltaR(wquark1) if jet.deltaR(wquark1) < JET_DR and np.abs(jet_flav) == np.abs(wquark1_pid) else 999
-#                 # wquark2_deltaR = jet.deltaR(wquark2) if jet.deltaR(wquark2) < JET_DR and np.abs(jet_flav) == np.abs(wquark2_pid) else 999
-#                 bquark_deltaR = jet.deltaR(bquark) if jet.deltaR(bquark) < JET_DR else 999
-#                 wquark1_deltaR = jet.deltaR(wquark1) if jet.deltaR(wquark1) < JET_DR else 999
-#                 wquark2_deltaR = jet.deltaR(wquark2) if jet.deltaR(wquark2) < JET_DR else 999
 
-#                 wquark_deltaR = wquark1_deltaR if wquark1_deltaR < wquark2_deltaR else wquark2_deltaR
-#                 min_j_deltaR, q = (bquark_deltaR, 1) if bquark_deltaR < wquark_deltaR else (wquark_deltaR, 2)  # 1 means bquark, 2 means wquark
+#         # matched_jet_set = {}
+#         # for i, bquark in enumerate(bquarks_event):
+#         #     bquark_deltaR = []
+#         bquark_deltaRs, w1quark_deltaRs, w2quark_deltaRs = [], [], []
+#         for jet_idx, (jet, jet_flav) in enumerate(zip(jets_event, jets_event.flavor)):  # Is jet_flav a useful quantity?
+#             bquark_deltaRs.append(jet.deltaR(bquarks_event))
+#             w1quark_deltaRs.append(jet.deltaR(wquarks1_event))
+#             w2quark_deltaRs.append(jet.deltaR(wquarks2_event))
+        
+#         matched_jet_set = {}
+#         for deltaRs in [bquark_deltaRs, w1quark_deltaRs, w2quark_deltaRs]:
 
-#                 if min_j_deltaR < mindeltaR:
-#                     mindeltaR = min_j_deltaR
-#                     mindeltaR_idxs = (j+1, q)  # j+1 b/c index top as 1, 2, 3, etc
+#             min1_deltaR, min1_idx, min2_deltaR, min2_idx = 999, -1, 999, -1
+#             for jet_idx, deltaR in enumerate(deltaRs):
+#                 for q, quark_dR in enumerate(deltaR):
+
+#                 if (
+#                     deltaR[0] < min1_deltaR and jet_idx not in matched_jet_set
+#                 ):
+#                     min1_deltaR = deltaR[0]
+#                     min1_idx = jet_idx
+#                 if (
+#                     deltaR[1] < min2_deltaR and jet_idx not in matched_jet_set
+#                 ) and (
+#                     jet_idx != min1_idx or deltaR[1] < deltaR[0]
+#                 ):
+#                     min1_deltaR = deltaR[0]
+#                     min1_idx = jet_idx
+#                 min1_deltaR = deltaR[0]  else min1_deltaR
+#                 min2_deltaR = deltaR[1] if deltaR[1] < min2_deltaR and jet_idx not in matched_jet_set else min2_deltaR
+#             min_deltaR = np.argmin(deltaRs, axis=0)
+
 
 #             if mindeltaR != 999:
 #                 alljet_match_idx = mindeltaR_idxs[0]