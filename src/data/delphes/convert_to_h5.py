20b7fd2a041aa635eb83023ed2d179ede828fea3
Thomas Sievert <tsievert@login-2.ultralight.org>
2026-02-26 01:29:24 -0800

    fixing bug 8

--- src/data/delphes/convert_to_h5.py
+++ src/data/delphes/convert_to_h5.py
@@ -601,6 +601,7 @@ def get_datasets(arrays, n_tops):  # noqa: C901
             | top_fullyBoosted[f"top{i+1}_mask"]
         ).astype("float32")
     print(f"Viable tops = {np.sum(at_least_one_target_mask)}")
+    at_least_one_target_mask = np.flatnonzero(at_least_one_target_mask)
 
     ## Check data ##
     # check fully-resolved tops
