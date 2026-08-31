fixed overflow bins in plotting

src/analysis/plot.py
@@ -64,7 +64,7 @@ def plot_pur_eff_w_dict(
         'FB': np.arange(200, 1000, 50),
         'all': np.arange(0, 1000, 50),
     }, 
-    plot_emu_boost: bool=True, plot_emu_semi: bool=True
+    plot_emu_boost: bool=False, plot_emu_semi: bool=False
 ):
 
     plot_bins_dict = {

src/analysis/utils.py
@@ -133,9 +133,10 @@ def calc_pureff(LUT, bins):
     Tops = np.array([top for top in LUT])
 
     Tops_inds = np.digitize(Tops[:, 1], bins)  # index 1 is pt
+    Tops_inds = ak.where(Tops[:, 1] < bins[-1], Tops_inds, bins[-1]+1)
 
     correctTruth_per_bin = []
-    for bin_i in range(1, len(bins) + 1):
+    for bin_i in range(len(bins)):
         correctTruth_per_bin.append(Tops[:, 0][Tops_inds == bin_i])  # index 0 is correct prediction
     correctTruth_per_bin = ak.Array(correctTruth_per_bin)
 