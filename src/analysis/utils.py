81f71b26deda27c8d623541f6cc18b576caedc37
Thomas Sievert <tsievert@login-2.ultralight.org>
2026-09-25 18:23:11 -0700

    changed the way to compute roc curves

--- src/analysis/utils.py
+++ src/analysis/utils.py
@@ -9,6 +9,19 @@ from hist.intervals import clopper_pearson_interval
 def n_alpha(string: str):
     return len([c for c in string if c.isalpha()])
 
+# def add_merged_selections(predfile, testfile, reconstructions: list[str], n_tops: int, selected_order: ak.Array, LUT_pred: ak.Array):
+#     valids = get_numerical(testfile, "MASK", reconstructions)
+#     for i, reco_class in enumerate(reconstructions):
+#         # print(ak.local_index(selected_order, axis=1))
+#         # print(ak.local_index(selected_order, axis=1)[selected_order == i])
+#         # print(ak.firsts(ak.local_index(selected_order, axis=1)[selected_order == i], axis=1))
+#         # print(ak.fill_none(ak.firsts(ak.local_index(selected_order, axis=1)[selected_order == i], axis=1), -1))
+#         predfile["TARGETS"][reco_class]["merged_order"] = ak.fill_none(ak.firsts(ak.local_index(selected_order, axis=1)[selected_order == i], axis=1), -1)
+#         predfile["TARGETS"][reco_class]["merged_correct"] = ak.fill_none(ak.firsts(LUT_pred[..., 0][selected_order == i], axis=1), -1)
+#         # merged_correct = ak.fill_none(ak.firsts(selected_order[selected_order == i], axis=1), -1)
+#         # predfile["TARGETS"][reco_class]["merged_correct"] = ak.where(merged_correct != -1, LUT_pred[:, 0], )
+#         predfile["TARGETS"][reco_class]["merged_valid"] = ak.any(valids[:, [j for j in range(len(reconstructions)) if j // n_tops == i // n_tops]], axis=1)
+
 def reset_collision_dp(dps, aps):
     ap_filter = aps < 1 / (13 * 13)
     return ak.where(ap_filter, 0, dps)
@@ -34,15 +47,15 @@ def get_jet4moms(testfile):
     return jets, fatjets
 
 
-def get_symmetries(recos, jet_labels):
+def get_symmetries(recos, jet_labels, returnidx: bool=True):
     symmetries = []
     for reco in recos:
-        unique_matches = [(i, i) for i, label in enumerate(jet_labels[reco]) if label.isalpha()]
-        sym_matches = [i for i, label in enumerate(jet_labels[reco]) if not label.isalpha()]
+        unique_matches = [(i, i) if returnidx else (label, label) for i, label in enumerate(jet_labels[reco]) if label.isalpha()]
+        sym_matches = [i if returnidx else label for i, label in enumerate(jet_labels[reco]) if not label.isalpha()]
         reco_syms = np.array([
-            np.vstack([np.array(unique_matches, dtype=int), np.hstack([np.array(sym_matches, dtype=int)[:, np.newaxis], np.array(sym_combo, dtype=int)[:, np.newaxis]])])
+            np.vstack([np.array(unique_matches, dtype=int if returnidx else str), np.hstack([np.array(sym_matches, dtype=int if returnidx else str)[:, np.newaxis], np.array(sym_combo, dtype=int if returnidx else str)[:, np.newaxis]])])
             for sym_combo in list(itertools.permutations(sym_matches, r=len(sym_matches)))
-        ], dtype=int)
+        ], dtype=int if returnidx else str)
         symmetries.append(reco_syms)
     return symmetries
 
